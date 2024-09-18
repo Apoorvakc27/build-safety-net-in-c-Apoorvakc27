56727c89bc78b7b860c35847275d7a82c7d9e0df
Apoorvakc27 <89474200+Apoorvakc27@users.noreply.github.com>
2024-09-18 16:08:43 +0530

    Update Soundex.h

--- Soundex.h
+++ Soundex.h
@@ -1,6 +1,12 @@
 #ifndef SOUNDEX_H
 #define SOUNDEX_H
 
+/* Function to generate the Soundex code */
+void generateSoundex(const char *name, char *soundex) {
+    int len = strlen(name);
+    int sIndex = 0;  
+}
+
 
 
 #endif // SOUNDEX_H
