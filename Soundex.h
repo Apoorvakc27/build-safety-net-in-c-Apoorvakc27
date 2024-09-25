06fa500734b41ce2312b9955c82867da8a047fac
Apoorvakc27 <89474200+Apoorvakc27@users.noreply.github.com>
2024-09-25 09:20:46 +0530

    Update Soundex.h

--- Soundex.h
+++ Soundex.h
@@ -1,10 +1,26 @@
 #ifndef SOUNDEX_H
 #define SOUNDEX_H
 
+/* Lookup table for Soundex codes for 'A' to 'Z' */
+char soundexMap[26] = {
+    '0', '1', '2', '3', '0', '1', '2', '0', '0', '2', // A-J
+    '2', '4', '5', '5', '0', '1', '2', '6', '2', '3', // K-T
+    '0', '1', '0', '2', '0', '2'                      // U-Z
+};
+
+char toUpperCase(char c) {
+    return toupper(c);
+}
+void initializeSoundex(const char *name, char *soundex, int *sIndex) {
+    soundex[0] = toUpperCase(name[0]);  
+    *sIndex = 1;  /* Initialize the index to the second character */
+}
+
 /* Function to generate the Soundex code */
 void generateSoundex(const char *name, char *soundex) {
     int len = strlen(name);
     int sIndex = 1;
+     initializeSoundex(name, soundex, &sIndex);  
 }
 
 
